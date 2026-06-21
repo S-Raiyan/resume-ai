24f1ef62b00ad987d36a6d0691345d0adcdedc5a
Ubuntu <ubuntu@ip-172-31-34-170.ap-south-1.compute.internal>
2026-06-21 17:06:43 +0000

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Route,Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/DashBoard";
+import Dashboard from "./pages/Dashboard";
 import Upload from "./pages/UploadResume";
 import ResumeResult from "./pages/ResumeResult";
 
@@ -13,7 +13,7 @@ export default function App(){
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/Register" element={<Register/>}/>
-      <Route path="Dashboard" element={<Dashboard/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/upload" element={<Upload/>}/>
       <Route path="/result" element={<ResumeResult/>}/>
 
